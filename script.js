801048091b891839900066a834b56969d8ecccc7
Gskars <112108655+Gskars@users.noreply.github.com>
2022-09-09 20:31:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var listaFilmes = [
+    "https://tse2.mm.bing.net/th?id=OIP.ALqeOHaWNBDkLd79-Wl8pAHaLH&pid=Api&P=0",
+    "https://tse2.mm.bing.net/th?id=OIP.tA0Efq4hGWOOuio8t0GgnAHaLG&pid=Api&P=0",
+    "https://i.pinimg.com/736x/15/47/17/154717f26b61d0dec2ea9e8d3fbe3de0.jpg#n=8824553882423",
+    "https://tse1.mm.bing.net/th?id=OIP.ZqjUANWpVJk669z7m3KqFQHaKj&pid=Api&P=0",
+    "http://www.postavy.cz/obrazky-thumbs/wiki/logan-190142-wikithumb.jpg",
+    "https://www.chunkys.com/nas/content/live/chunkys/wp-content/uploads/2021/11/Spider-Man-No-Way-Home-Poster-1.jpg",
+    "https://tse4.mm.bing.net/th?id=OIP.d51UrfbMnMFyTNHwxgxSvgHaLH&pid=Api&P=0",
+    
+ ]
+ 
+ 
+ for (var i = 0; i < listaFilmes.length; i++) {
+     document.write("<img src=" + listaFilmes[i] + ">");
+   }
